6366d9d7b8cb2491d9890df41009e3ee2fd3dce4
Adrian R <roblesadrian03@gmail.com>
2024-02-14 17:44:32 +0100

    Actualizado constructor Parcela

--- src/Parcela.java
+++ src/Parcela.java
@@ -8,14 +8,14 @@ public class Parcela {
     private int numJardines;
     private boolean tieneGaraje; 
     private int numBanos;
+    private boolean enVenta;
 
-    
 
     public Parcela() {
 
         char tempTipo = 'A';
         int autogenTipo = (int) (Math.random() * 100) + 1; 
-
+    
         if(autogenTipo > 0 && autogenTipo <= 50) {
             tempTipo = 'C'; 
         } else if (autogenTipo > 50 && autogenTipo <= 85) {
@@ -24,22 +24,23 @@ public class Parcela {
             tempTipo = 'P';
         }
         
-
         this.tipo = tempTipo;
+        
+        if (this.tipo == 'C') { // Si la parcela es una casa
+            this.numBanos = (int) (Math.random() * 2 + 2); // Generar número de baños
+            this.tieneGaraje = Math.random() < 0.5; // Generar si tiene garaje o no
+        } else { // Si la parcela no es una casa
+            this.numBanos = 0;
+            this.tieneGaraje = false;
+        }
+        
         this.precio = PRECIO_BASE;
         this.numParques = (int) (Math.random() * 2 + 1);
         this.numJardines = (int) (Math.random() * 2 + 1);
-
-        int autogeneradorGaraje = (int) (Math.random() * 2) + 1; 
-
-        if(autogeneradorGaraje == 1) {
-            this.tieneGaraje = true; 
-        } else if (autogeneradorGaraje == 2) {
-            this.tieneGaraje = false; 
-        } 
-
-        this.numBanos = (int) (Math.random() * 2 + 2);
+        
+        this.enVenta = true; // Indicar que la parcela está en venta inicialmente
     }
+    
 
     public char getTipo() {
         return tipo;
@@ -88,6 +89,15 @@ public class Parcela {
     public void setNumBanos(int numBanos) {
         this.numBanos = numBanos;
     } 
+
+    public boolean isEnVenta() {
+        return enVenta;
+    }
+
+    public void setEnVenta(boolean enVenta) {
+        this.enVenta = enVenta;
+    }
+
 
     @Override
     public String toString() {
